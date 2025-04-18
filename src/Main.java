c33265cc38cd512495ea72cf44b15948e2dc4ae4
MarlonVieira <marlongabriel_100@hotmail.com>
2025-04-17 21:42:48 -0300

    Classroom of: Controlling access and writing to application data. 17/04/2025

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
+import br.com.alura.screenmatch.model.Movie;
+
 public class Main {
     public static void main(String[] args) {
         Movie myMovie = new Movie();
 
-        myMovie.name = "Pulp Fiction";
-        myMovie.releaseYear = 1994;
-        myMovie.durationInMinutes = 154;
-        myMovie.includedInPlan = true;
+        myMovie.setName("Pulp Fiction");;
+        myMovie.setReleaseYear(1994);
+        myMovie.setDurationInMinutes(154);
+        myMovie.setIncludedInPlan(true);
         myMovie.rate(10);
         myMovie.rate(8.9);
         myMovie.rate(7.8);
         myMovie.showTechnicaSheet();
-        System.out.println(myMovie.totalRatings);
-        System.out.println(myMovie.sumRatings);
         System.out.println("The average rating of the movie is: " + myMovie.averageRating());
     }
 }
---
+++ src/br/com/alura/screenmatch/model/Movie.java
@@ -0,0 +1,64 @@
+package br.com.alura.screenmatch.model;
+
+public class Movie {
+
+    private String name = "";
+    private int releaseYear = 0;
+    private boolean includedInPlan = false;
+    private double sumRatings = 0;
+    private int totalRatings = 0;
+    private int durationInMinutes = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public boolean getIncludedInPlan() {
+        return includedInPlan;
+    }
+
+    public void setIncludedInPlan(boolean includedInPlan) {
+        this.includedInPlan = includedInPlan;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
+    public int getTotalRatings() {
+        return totalRatings;
+    }
+
+    public void showTechnicaSheet() {
+        System.out.println("Name of the movie: " + getName());
+        System.out.println("Release year: " + getReleaseYear());
+        System.out.println("Duration in minutes: " + getDurationInMinutes());
+        System.out.println("Included in plan: "+ getIncludedInPlan());
+        System.out.println("Total Ratings: " + getTotalRatings());
+    }
+
+    public void rate(double evaluation){
+        sumRatings += evaluation;
+        totalRatings++;
+    }
+
+    public double averageRating() {
+        return sumRatings / getTotalRatings();
+    }
+}
