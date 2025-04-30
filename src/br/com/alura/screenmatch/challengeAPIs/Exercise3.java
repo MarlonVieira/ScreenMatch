7ef30a92a27da785ab839873c36c9b23ab6cc35e
Marlon_Vieira <marlon.vieira@viasoft.com.br>
2025-04-30 09:01:38 -0300

    Lesson 01. Requests and responses. Exercises 2 and 3.
    04/30/2024

--- src/br/com/alura/screenmatch/challengeAPIs/Exercise3.java
+++ src/br/com/alura/screenmatch/challengeAPIs/Exercise3.java
@@ -7,18 +7,15 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class Exercise2 {
+public class Exercise3 {
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter the currency conversion: ");
-        String currency = scan.nextLine();
+            System.out.print("Enter the desired recipe: ");
+        String search = scan.nextLine();
 
-        System.out.print("Enter the desired crypto coin: ");
-        String crypto = scan.nextLine();
-
-        String address = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" +currency+ "&ids=" +crypto+ "&x_cg_demo_api_key=CG-rtTddrVLUknWiWGLNpEv4XMC";
+        String address = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + search;
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(address))
@@ -26,6 +23,6 @@ public class Exercise2 {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+            System.out.println(response.body());
     }
 }
